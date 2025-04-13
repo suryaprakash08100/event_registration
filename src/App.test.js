2d585542e50d5cff6a22da1cac3ac57b034ece66
suryaprakash08100 <prakash.komati@sasi.ac.in>
2025-04-13 20:13:37 +0530

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+// src/App.test.js
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders event registration form', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Event Registration/i);
+  expect(headingElement).toBeInTheDocument();
 });
